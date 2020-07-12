21f1a6eb73e68f1b2b9faf7c9580a251e5edf000
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2020-07-12 23:13:49 +0500

    Lesson 4 begins

---
+++ VK_App_SB/Custom Views/ShadowView.swift
@@ -0,0 +1,9 @@
+//
+//  ShadowView.swift
+//  VK_App_SB
+//
+//  Created by Dmitrii Diadiushkin on 12.07.2020.
+//  Copyright © 2020 Dmitrii Diadiushkin. All rights reserved.
+//
+
+import Foundation
